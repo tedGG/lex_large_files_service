e86d13b473fb68bd6b39b87ef70918c85c4b1b75
Nazar Kovalchuk <nazar05548@gmail.com>
2026-01-16 15:49:10 +0200

    get file salesforce

--- serviceUploader/salesforce.js
+++ serviceUploader/salesforce.js
@@ -7,11 +7,11 @@ class SalesforceConnection {
   async getToken(basicUrl){
     const url = basicUrl + '/services/oauth2/token';
     //const url = 'https://nosoftware-computing-6653--qa.sandbox.my.salesforce.com/services/oauth2/token';
- console.log('getToken 1111111', url);
- console.log('getToken 1111111', process.env.CLIENT_ID_SF);
- console.log('getToken 1111111', process.env.CLIENT_SECRET_SF);
+    console.log('getToken 1111111', url);
+    console.log('getToken 1111111', process.env.CLIENT_ID_SF);
+    console.log('getToken 1111111', process.env.CLIENT_SECRET_SF);
  
- const params = new URLSearchParams();
+    const params = new URLSearchParams();
     params.append('grant_type', 'client_credentials'); 
     params.append('client_id', process.env.CLIENT_ID_SF);
     params.append('client_secret', process.env.CLIENT_SECRET_SF);
@@ -19,7 +19,8 @@ class SalesforceConnection {
     const response = await axios.post(url, params, {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
     });
-console.log('getToken 22222');
+    
+    console.log('getToken 22222');
     console.log('Access Token:', response.data.access_token);
     return response.data.access_token;
   }
