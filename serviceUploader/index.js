d855a4b830bf586beb1e16331b3e862cb8708d3a
Nazar Kovalchuk <nazar05548@gmail.com>
2026-01-16 15:34:42 +0200

    debug logs

--- serviceUploader/index.js
+++ serviceUploader/index.js
@@ -18,16 +18,17 @@ app.get("/wakeup", async (req, res) => {
 
 app.get("/salesforce", async (req, res) => {
   console.log('/salesforce');
+  console.log('Success request!');
   const basicUrl = req.headers['basicurl'];
   const sharepointEndpoint = req.headers['sharepointendpoint'];
   const contVerId = req.headers['contverid'];
   console.log('OUTPUT : ',basicUrl);
-  const token = await sharepoint.getAccessToken();
- console.log('token : ',token);
-  const file = await salesforce.getFile(basicUrl, contVerId);
-console.log('file : ',file);
-  const result = await sharepoint.createFile(file, token, sharepointEndpoint);
-console.log('result : ',result);
+  // const token = await sharepoint.getAccessToken();
+  // console.log('token : ',token);
+  // const file = await salesforce.getFile(basicUrl, contVerId);
+  // console.log('file : ',file);
+  // const result = await sharepoint.createFile(file, token, sharepointEndpoint);
+  // console.log('result : ',result);
   res.send("salesforce");
 });
 
